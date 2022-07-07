fa510c35076ca0577768fd051e1a2e6e6756e4bf
William Douglas <william.douglas@intel.com>
2022-07-07 13:58:44 -0700

    rework update.sh for make update

--- Makefile
+++ Makefile
@@ -2,9 +2,3 @@ PKG_NAME := clr-hardware-files
 VERSION := 1
 
 include ../common/Makefile.common
-
-update:
-	echo Downloading new content
-	curl -sSf http://pciids.sourceforge.net/v2.2/pci.ids -o pci.ids
-	curl -sSf http://www.linux-usb.org/usb.ids -o usb.ids
-	git diff --exit-code pci.ids usb.ids || bash ./update.sh
--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+set -e -o pipefail
+
+curl -sSf http://pciids.sourceforge.net/v2.2/pci.ids -o pci.ids
+curl -sSf http://www.linux-usb.org/usb.ids -o usb.ids
+git diff --exit-code pci.ids usb.ids && exit
+
 make bumpnogit
 git commit -m "device ID updates" pci.ids usb.ids *.spec release
 make koji-nowait
