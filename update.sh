253618171561d8c1645146cca647f2730bf574c9
Patrick McCarty <patrick.mccarty@intel.com>
2022-09-27 20:46:37 +0000

    follow redirects
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 set -e -o pipefail
 
-curl -sSf https://pciids.sourceforge.net/v2.2/pci.ids -o pci.ids
-curl -sSf http://www.linux-usb.org/usb.ids -o usb.ids
+curl -sSf -L https://pciids.sourceforge.net/v2.2/pci.ids -o pci.ids
+curl -sSf -L http://www.linux-usb.org/usb.ids -o usb.ids
 git diff --exit-code pci.ids usb.ids && exit
 
 make bumpnogit
